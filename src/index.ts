85acc5fa259f657cb3e7121fa45c5a7fefcf1b2a
happyjino <wlsgh1128@naver.com>
2023-05-03 12:32:59 +0900

    ch02_2(완료)

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,8 @@
-const testMakePerson = (): void => {
-  let jane: IPerson = makePerson('Jane');
-  let jack: IPerson = makePerson('Jack');
-  console.log(jane, jack)
-}
+import IPerson from './person/IPerson';
+import Person from './person/Person';
+import Chance from 'chance';
+import * as R from 'ramda'
 
-testMakePerson();
+const chance = new Chance();
+let persons: IPerson[] = R.range(0, 2).map((n: number) => new Person(chance.name(), chance.age()));
+console.log(persons)
